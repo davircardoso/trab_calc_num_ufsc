60c4737ada14c3f0c7cfca4a3ee8cb18534ebc14
Davi Cardoso <davir@kyros.com.br>
2021-04-03 16:57:52 -0300

    1.4 e 1.5inicial

--- q2.py
+++ q2.py
@@ -43,10 +43,17 @@ def false_position(f, a, b, tol):
     return p, count, x
 
 def newton(f, Df, x0, eps):
+    t_residual_newt = []
+    r_residual_newt = []
+    tk = []
     xn = x0
     fxn = f(xn)
     count = 0
+    tk.append(xn)
     while abs(fxn) > eps:
+        tk.append(xn)
+        t_residual_newt.append(xn)
+        r_residual_newt.append(f(xn))
         count += 1
         fxn = f(xn)
         Dfxn = Df(xn)
@@ -54,13 +61,17 @@ def newton(f, Df, x0, eps):
             print('Zero derivative. No solution found.')
             return None
         xn = xn - fxn/Dfxn
-    return xn, count
+    return xn, count, tk, t_residual_newt, r_residual_newt
 
 def secant(f, a, b, eps):
     f_a = f(a)
     f_b = f(b)
+    ta = []
+    tb = []
     count = 0
     while abs(f_b) > eps:
+        ta.append(a)
+        tb.append(b)
         count += 1
         denominator = float(f_b - f_a) / (b - a)
         p = b - float(f_b) / denominator
@@ -68,7 +79,7 @@ def secant(f, a, b, eps):
         b = p
         f_a = f_b
         f_b = f(b)
-    return p, count
+    return p, count, ta, tb
 
 tol = 10**(-6) # tolerância máxima
 bisec = bisection(f, 2, 6, tol)
@@ -98,6 +109,8 @@ bisec_k = range(len(bisec_a)) # lista de iterações (0 a n)
 
 # exibição do gráfico
 
+bisec_graph_ka = plt.plot(bisec_k, bisec_a)
+bisec_graph_kb = plt.plot(bisec_k, bisec_b)
 bisec_graph_ka = plt.scatter(bisec_k, bisec_a)
 bisec_graph_kb = plt.scatter(bisec_k, bisec_b)
 plt.grid(True)
@@ -114,10 +127,71 @@ fp_k = range(len(fp_a)) # lista de iterações (0 a n)
 
 # exibição do gráfico
 
+fp_graph_ka = plt.plot(fp_k, fp_a)
+fp_graph_kb = plt.plot(fp_k, fp_b)
 fp_graph_ka = plt.scatter(fp_k, fp_a)
 fp_graph_kb = plt.scatter(fp_k, fp_b)
 plt.grid(True)
 plt.legend((fp_graph_ka, fp_graph_kb), ('a', 'b'), loc='upper right')
 plt.title('Método da falsa posição')
 plt.xlabel('k')
+plt.show()
+
+# método de newton
+
+list_count_newt = list(range(newt[1]+1))
+
+plt.xlabel('K iterações')
+plt.ylabel('Tk')
+plt.title('Tk Método de Newton')
+plt.grid(True)
+plt.gca().set_xlim([0, 25])
+plt.gca().set_ylim([1.5, 4])
+plt.plot(list_count_newt, newt[2], c='b')
+plt.scatter(list_count_newt, newt[2], c='r')
+plt.show()
+
+# método da secante
+
+list_count_sec = list(range(sec[1]))
+
+plt.xlabel('K iterações')
+plt.ylabel('Ta e Tb')
+plt.title('Ta e Tb Método da Secante')
+plt.grid(True)
+plt.gca().set_xlim([0, 7])
+plt.gca().set_ylim([-5, 7])
+ta = plt.plot(list_count_sec, sec[2], c='b')
+tb = plt.plot(list_count_sec, sec[3], c='orange')
+ta = plt.scatter(list_count_sec, sec[2], c='b')
+tb = plt.scatter(list_count_sec, sec[3], c='orange')
+plt.legend((ta, tb), ('Ta', 'Tb'), loc='upper right')
+plt.show()
+
+# 1.5 - Newton
+
+tempo = list(range(10))
+
+plt.figure(figsize=(15, 15))
+plt.subplot(4,2,1)
+plt.xlabel('tempo')
+plt.ylabel('R(t) e aproximação')
+plt.title('Tk Método de Newton')
+plt.grid(True)
+plt.gca().set_xlim([1.5, 5])
+plt.gca().set_ylim([-0.5, 0.6])
+plt.plot(tempo, tempo, c='g')
+plt.plot(newt[3], newt[4], c='r')
+plt.scatter(newt[3], newt[4], c='b')
+
+
+plt.subplot(4,2,2)
+for i in range(len(newt[2])):
+    newt[2][i] = newt[2][i] -
+plt.plot(newt[1], newt[2], c='g')
+plt.scatter(newt[1], newt[2] , c='r')
+plt.xlabel('iterações')
+plt.ylabel('Aproximação do tempo de pico')
+plt.title('Aproximação ao tempo de pico Método de Newton')
+plt.grid(True)
 plt.show()
